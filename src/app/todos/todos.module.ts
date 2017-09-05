13c1cdb7b85fbbd8133eda7798e8df70a199ce76
Elger van Boxtel <elgervb@gmail.com>
2017-09-05 22:05:13 +0200

    sync with localstorage

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,8 +1,10 @@
+import { ActionWithPayload, TodoItem, TodoState } from './todos.models';
 import { TodosService } from './todos.service';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, ActionReducerMap, ActionReducer, Action } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 import { TodosRoutingModule } from './todos-routing.module';
 import { ListComponent } from './list/list.component';
@@ -11,11 +13,21 @@ import { TodosComponent } from './todos.component';
 import { ItemFormComponent } from './item-form/item-form.component';
 import { todosReducer } from './todos.reducers';
 
+const reducers: ActionReducerMap<TodoState> = {todos: todosReducer};
+
+export function localStorageSyncReducer(reducer): ActionReducer<TodoState, ActionWithPayload<TodoItem>> {
+  return localStorageSync({
+    keys: ['todos'],
+    rehydrate: true
+  })(reducer);
+}
+const metaReducers = [localStorageSyncReducer];
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    StoreModule.forRoot({todos: todosReducer}),
+    StoreModule.forRoot(reducers, {metaReducers}),
     TodosRoutingModule
   ],
   declarations: [ListComponent, ListItemComponent, TodosComponent, ItemFormComponent],
