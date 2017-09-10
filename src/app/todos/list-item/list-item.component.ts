61916da6c17f4bd726ba4337d04c5567356d9452
Elger van Boxtel <elgervb@gmail.com>
2017-09-10 14:18:25 +0200

    add check component

--- src/app/todos/list-item/list-item.component.ts
+++ src/app/todos/list-item/list-item.component.ts
@@ -6,7 +6,9 @@ import { TodoItem } from '../todos.models';
   styleUrls: ['list-item.component.scss'],
   template: `
     <div class="item" (click)="showDetails.emit(todo)">
-      <i class="status" [class.completed]="todo.completed" (click)="complete.emit(todo)"></i>
+      <i class="status" [class.completed]="todo.completed" (click)="complete.emit(todo); $event.stopPropagation();">
+        <span class="mark">✔</span>
+      </i>
       <p class="todo__title">{{todo.todo}}</p>
       <button class="btn__delete" (click)="toggleDelete();$event.stopPropagation()">&times;</button>
       <div class="item__delete" [class.show]="showDelete" (click)="delete.emit(todo)" (mouseout)="toggleDelete(false)">delete</div>
